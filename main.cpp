e89f992742ed062265e89f5d71fed87cdb307705
Helmi <rumahhelmi@gmail.com>
2025-06-19 13:44:37 +0800

    Membuat fungsi pop
    -Memperbaiki bug memory leak (fungsi exit)
    
    -Menambahkan documentasi awal

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<ctime>
 #include<cstdlib>
 
+// Struct data
 struct Node{
   int id;
   std::string name;
@@ -11,58 +12,84 @@ struct Node{
   std::time_t time;
   Node *next;
 
-  Node(): next(nullptr), time(std::time(NULL)){};
+  Node(): time(std::time(NULL)), next(nullptr){};
 };
 
+// Struct Queue menggunakan linkedlist
 struct Queue{
   Node *head;
   Node *tail;
 
+  // Constructor untuk mendefinisikan default head dan tail;
   Queue(): head(nullptr), tail(nullptr){};
+  // Destructor untuk handle memory safety
   ~Queue(){
-    while(head != nullptr){
+    while(head != nullptr)
+    {
       Node *tmp = head;
       head = head->next;
       delete tmp;
     }
     tail = nullptr;
+    std::cout << "Success handle memory safety" << std::endl;
   }
 
-  void _checkNull();
-  void _insert(size_t index, std::string name, int phoneNum, std::string desc);
-  void remove();
+  // Mendefinisasikan fungsi dari Queue
+  // Note: fungsi yang bukan original dari Queue didefinisasikan dengan tanda "_"
+  bool _isNull();
+  void _insert(const size_t index, const std::string &name, const int phoneNum, const std::string &desc);
+  void _remove(size_t index);
   void pop();
-  void push(std::string name, int phoneNum, std::string desc);
-  void front();
-  void back();
+  void push(const std::string &name, const int phoneNum, const std::string &desc);
+  // void front();
+  // void back();
   void show();
   size_t size();
-
 };
 
 int main(){
-
+  Queue test;
+  test.show();
   return 0;
 }
 
+
 size_t Queue::size(){
+  /* 
+    Fungsi yang mengembalikan jumlah elemen dari Queue.
+    Dengan cara melakukan pengecekan berulang pada setian node next.
+  */
+
   size_t i = 0;
   Node *current = head;
-  while(head != nullptr){
+  while(current != nullptr)
+  {
     ++i;
     current = current -> next;
   }
   return i;
 }
 
-void Queue::_checkNull(){
-  if(head == nullptr){
-    std::cerr << "Error: Require data not satisfied, abort program instead." << std::endl;
-    exit(EXIT_FAILURE);
+
+bool Queue::_isNull(){
+  /* 
+   Fungsi untuk pengecekan apakah data kosong atau tidak
+  */
+
+  if(head == nullptr)
+  {
+    std::cerr << "Error: Require data not satisfied, abort function instead!" << std::endl;
+    return true;
   }
+  return false;
 }
 
-void Queue::push(std::string name, int phoneNum, std::string desc){
+
+void Queue::push(const std::string &name, const int phoneNum, const std::string &desc){
+  /*
+    Fungsi untuk melakukan push data
+    */
+
   Node *newNode = new Node();
   time_t currentTimeStamp;
 
@@ -71,66 +98,131 @@ void Queue::push(std::string name, int phoneNum, std::string desc){
   newNode->desc = desc;
   newNode->time = time(&currentTimeStamp);
 
-
+  // Handle jika pertama kali push(index 0)
   if(head == nullptr){
     head = newNode;
+    tail = newNode;
     return;
   }
   
-  Node *tmp = head;
-  while(tmp -> next != nullptr){
-    tmp = tmp -> next;
+  Node *current = head;
+  while(current -> next != nullptr){
+    current = current -> next;
   }
-  tmp -> next = newNode;
+  current -> next = newNode;
 }
+
 
 void Queue::pop(){
-  _checkNull();
-  Node *tmp = head -> next;
-  delete head;
-  head = tmp;
+  /*
+    Fungsi untuk melakukan pop data
+  */
+
+
+  if(_isNull())
+  {
+    return;
+  }
+
+  Node *tmp = head;
+  // Handle jika yang di pop merupakan data terakhir
+  if(tmp == tail)
+  {
+    tail = nullptr;
+  }
+  head = head->next;
+  delete tmp;
 }
 
-void Queue::remove(){}
+
+void Queue::_remove(const size_t index){
+  /*
+    Fungsi untuk melakukan remove pada indeks tertentu
+    Fungsi ini memiliki behavior dimana dia tidak bisa melakukan fungsionalitas yang sama dengan pop
+  */
+
+  std::cerr << "Warning: Remove isn't a original function from Queue" << std::endl;
+
+  if(_isNull())
+  {
+    return;
+  }
+  else if(index == 0 || index > size())
+  {
+    std::cerr << "Error: Unexpected behavior, use pop instead!" << std::endl;
+    return;
+  }
+  
+  Node *current = head;
+
+  for(size_t i = 1; i < index && current != nullptr; i++) // i dimulai dari satu karena current dimulai dari head
+  {
+    current = current->next;
+  }
+  
+  if(current->next == tail)
+  {
+    tail = current;
+  }
+  Node *tmp = current->next;
+  current->next = tmp->next;
+  delete tmp;
+}
+
 
 void Queue::show(){
-    _checkNull();
-    Node* current = head;
-    while (current != nullptr)
-    {
-        std::cout << "nama: " << current->name << std::endl;
-        std::cout << "No Telepon: " << current->phoneNum << std::endl;
-        std::cout << "Deskripsi: " << current->desc << std::endl;
-        std::cout << "Waktu Antri: " << ctime(&current->time) << std::endl;
-        current = current->next;
-    }
+  /*
+    Fungsi untuk menampilkan keseluruhan data
+  */
+
+  if(_isNull())
+  {
+    return;
+  }
+  Node* current = head;
+  size_t i = 1;
+  while (current != nullptr)
+  {
+    std::cout << "Data ke-" << i << std::endl;
+    std::cout << "nama: " << current->name << std::endl;
+    std::cout << "No Telepon: " << current->phoneNum << std::endl;
+    std::cout << "Deskripsi: " << current->desc << std::endl;
+    std::cout << "Waktu Antri: " << ctime(&current->time) << std::endl;
+    current = current->next;
+    ++i;
+  }
 }
 
-void Queue::_insert(size_t index, std::string name, int phoneNum, std::string desc) {
-    std::cerr << "Warning: Insert bukan merupakan fungsi murni dari Queue" << std::endl;
-    const size_t SIZE = size();
-    if(index >= SIZE - 1 || SIZE == 0){
-      std::cerr << "Error: Unexpected Behavior, Use push instead!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
 
-    time_t currentTimeStamp;
-    Node* newNode = new Node();
+void Queue::_insert(const size_t index, const std::string &name, const int phoneNum, const std::string &desc) {
+  /*
+    Fungsi untuk memasukkan data pada indeks tertentu
+    Fungsi ini memiliki behavior dimana dia tidak bisa melakukan fungsionalitas yang sama dengan push
+  */
 
-    newNode->name = name;
-    newNode->desc = desc;
-    newNode->phoneNum = phoneNum;
-    newNode->time = time(&currentTimeStamp);
+  std::cerr << "Warning: Insert isn't a original function from Queue" << std::endl;
 
-    Node* current = head;
+  if(index >= size() - 1 || head == nullptr)
+  {
+    std::cerr << "Error: Unexpected Behavior, Use push instead!" << std::endl;
+    return;
+  }
 
-    for(size_t i = 0; i < index && current != nullptr; i++)
-    {
-      current = current->next;
-    }
+  time_t currentTimeStamp;
+  Node* newNode = new Node();
 
-    newNode->next = current->next;
-    current->next = newNode;
+  newNode->name = name;
+  newNode->desc = desc;
+  newNode->phoneNum = phoneNum;
+  newNode->time = time(&currentTimeStamp);
+
+  Node* current = head;
+  
+  for(size_t i = 1; i < index && current != nullptr; i++) // i dimulai dari satu karena current dimulai dari head
+  {
+    current = current->next;
+  }
+
+  newNode->next = current->next;
+  current->next = newNode;
 }
-
-void initAntrian(){}
